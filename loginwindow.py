53f75a5618b385954e9fcd113b397eae8429fd3d
Markus Helin <markus@netalia.se>
2019-12-10 12:03:12 +0100

    connect loginwindow with API

--- loginwindow.py
+++ loginwindow.py
@@ -1,9 +1,16 @@
-from PySide2.QtGui import QGuiApplication, QIcon, QDesktopServices
-from PySide2.QtCore import Qt, QUrl, QThread, Slot, SIGNAL, QObject , QCoreApplication
-from PySide2.QtWidgets import QSystemTrayIcon, QMenu, QMessageBox, QApplication, QWidget, QVBoxLayout, QPushButton, QPlainTextEdit, QLabel, QMainWindow, QListWidget, QListWidgetItem
+from PySide2.QtGui import QIcon, QPixmap
+from PySide2.QtCore import Qt, QObject , QCoreApplication
+from PySide2.QtWidgets import QMessageBox, QApplication, QPushButton, QDialog, QLabel, QLineEdit, QVBoxLayout
 from config import Config
+from timerwindow import TimerWindow
+from api import API
+import logging
 
-class LoginWindow(QWidget):
+
+class LoginWindow(QDialog):
+
+    api = API()
+
 
     def __init__(self):
         super().__init__()
@@ -16,13 +23,41 @@ class LoginWindow(QWidget):
 
     def initUI(self):
         self.setWindowTitle(self.title)
-        #self.setWindowIcon(QIcon(Config.icon))
-        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.setWindowIcon(QIcon(Config.icon))
+        #self.setGeometry(self.left, self.top, self.width, self.height)
+ 
+        label = QLabel()
+        label.setPixmap(QPixmap('icon.png'))
+        label.setAlignment(Qt.AlignCenter)
         
-        self.button = QPushButton('Start', self)
-        self.button.move(150,20)
-        self.button.resize(100,48)
-        #self.button.clicked.connect(self.clickStart)
+        self.username = QLineEdit(self)
+        self.password = QLineEdit(self)
+        self.password.setEchoMode(QLineEdit.Password)
+        self.button_login = QPushButton('Login', self)
+        self.button_login.clicked.connect(self.handleLogin)
+        
+        layout = QVBoxLayout(self)
+        layout.addWidget(label)
+        layout.addWidget(self.username)
+        layout.addWidget(self.password)
+        layout.addWidget(self.button_login)
+        
         
         self.show()
+
+    def handleLogin(self):
+        data = self.api.auth(self.username.text(), self.password.text())
+        print(data)
+        try:
+            if data.get('username'):
+                logging.info("username and password accepted")
+                self.accept()
+            elif data.get('error'):
+                logging.info("username and password is incorrect")
+                QMessageBox.warning(
+                    self, 'Error', data['error'])
+        except (TypeError, AttributeError):
+            logging.error("Unknown error not cover in API() class")
+            QMessageBox.warning(self, 'Error', "Unknown error not cover in API() class")
+
 
