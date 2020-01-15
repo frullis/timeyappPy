daab46f5241e39838c8c610519bc22ded48232d6
Markus Helin <markus@netalia.se>
2020-01-15 17:10:22 +0100

    remove api_token

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 class Config:
     dashboard_url = 'https://www.google.com'
+    editaddtime_url = 'http://www.google.com?editaddtime'
+    icon = "icon.png"
+    title = "Timey app"
     api_endpoint = 'https://127.0.0.1:5000'
-    api_token = 'fea20970f5bb1b75c96dfa8985fd15b2a3c0f8a8d3261381d0176a05475781ee88d9f7252511e5e085b99e1cee37efa86f7364b7ed5203bccd2c2fd9b76057fe'
+    #api_token = 'asd'
+    #api_token = 'fea20970f5bb1b75c96dfa8985fd15b2a3c0f8a8d3261381d0176a05475781ee88d9f7252511e5e085b99e1cee37efa86f7364b7ed5203bccd2c2fd9b76057fe'
+    idletime = 1200 # 1200 = 20minutes
+    storage_dir = "storage/"
